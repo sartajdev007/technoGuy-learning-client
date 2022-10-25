6d33d375b78776238b084a26f22436a3ae108ba7
sartajdev007 <sartaj5akik@gmail.com>
2022-10-25 18:39:53 +0600

    layout and routes  added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,1 +1,27 @@
-export const routes = []
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from '../components/Home/Home';
+import Courses from '../components/Courses/Courses';
+import Login from '../components/Login/Login';
+
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    }
+])
