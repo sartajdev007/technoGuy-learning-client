2eda657a63208ad85bb26a7ea6d12a04fde21b6b
sartajdev007 <sartaj5akik@gmail.com>
2022-10-26 21:37:46 +0600

    registration system add

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const Register = () => {
+    const { createUser } = useContext(AuthContext)
+    const [error, setError] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user
+                console.log(user)
+                form.reset()
+            })
+            .catch(error => {
+                console.error(error)
+                setError(error.message)
+            })
+    }
+
+
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-green-600/40 ring-2 ring-green-600 lg:max-w-xl">
                 <h1 className="text-3xl font-semibold text-center text-green-700  uppercase">
                     Sign UP
                 </h1>
-                <form className="mt-6">
+                <form onSubmit={handleSubmit} className="mt-6">
                     <div className="mb-2">
                         <label
                             for="name"
@@ -18,6 +45,7 @@ const Register = () => {
                         </label>
                         <input
                             type="name"
+                            required
                             name='name'
                             className="block w-full px-4 py-2 mt-2 text-green-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
@@ -66,6 +94,9 @@ const Register = () => {
                             Sign UP
                         </button>
                     </div>
+                    <p className='text-red-500'>
+                        {error}
+                    </p>
                 </form>
 
                 <p className="mt-8 text-xs font-bold text-center text-gray-700">
---
+++ src/context/AuthProvider/AuthProvider.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
+import app from '../../firebase/firebase.config'
+import { useState } from 'react';
+
+export const AuthContext = createContext();
+const auth = getAuth(app)
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile)
+    }
+
+    const authInfo = { user, createUser, updateUserProfile }
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+
+
+export default AuthProvider;
